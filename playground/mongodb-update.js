43e62eb69f044b0aae0d64af13b0e7412fe21004
German <germanhernandez.dr@gmail.com>
2017-10-04 16:53:19 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59d4ffe8f312202e70ced145')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59d4fdd1f312202e70ced13e')
+  }, {
+    $set: {
+      name: 'Nito'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
