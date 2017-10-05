f1bda542d8e5eadaff41ae6078f28399835bbd79
German <germanhernandez.dr@gmail.com>
2017-10-05 14:40:56 +0100

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const { ObjectId } = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('59d6342a0c8ce40d8be1c488').then((todo) => {
+  console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -49,6 +49,24 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
